8d5b1457c2263fa5d6bd773239b57cb6bad21494
TJ Holowaychuk <tj@vision-media.ca>
2014-09-08 14:26:11 -0700

    rename .Query() to .Find() (breaking change)

--- hosts.go
+++ hosts.go
@@ -17,46 +17,46 @@ func New(auth aws.Auth, region aws.Region) *Client {
 
 // All returns all instances.
 func (c *Client) All() ([]Instance, error) {
-	return c.Query().Get()
+	return c.Find().Get()
 }
 
 // Running instances.
 func (c *Client) Running() ([]Instance, error) {
-	return c.Query().State("running").Get()
+	return c.Find().State("running").Get()
 }
 
 // Pending instances.
 func (c *Client) Pending() ([]Instance, error) {
-	return c.Query().State("pending").Get()
+	return c.Find().State("pending").Get()
 }
 
 // Terminated instances.
 func (c *Client) Terminated() ([]Instance, error) {
-	return c.Query().State("terminated").Get()
+	return c.Find().State("terminated").Get()
 }
 
 // Stopped instances.
 func (c *Client) Stopped() ([]Instance, error) {
-	return c.Query().State("stopped").Get()
+	return c.Find().State("stopped").Get()
 }
 
 // Id running instances with the given id(s).
 func (c *Client) Id(id ...string) ([]Instance, error) {
-	return c.Query().State("running").Id(id...).Get()
+	return c.Find().State("running").Id(id...).Get()
 }
 
 // Name running instances with the given name(s).
 func (c *Client) Name(name ...string) ([]Instance, error) {
-	return c.Query().State("running").Name(name...).Get()
+	return c.Find().State("running").Name(name...).Get()
 }
 
 // Tag running  instances with the given tag and value(s).
 func (c *Client) Tag(name string, value ...string) ([]Instance, error) {
-	return c.Query().State("running").Tag(name, value...).Get()
+	return c.Find().State("running").Tag(name, value...).Get()
 }
 
-// Query returns a chainable query.
-func (c *Client) Query() *Query {
+// Find returns a chainable query.
+func (c *Client) Find() *Query {
 	return &Query{
 		client: c,
 		filter: ec2.NewFilter(),
